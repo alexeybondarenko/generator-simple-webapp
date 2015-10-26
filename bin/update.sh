f33aa93fb491e12771beefb0e98fb6691861f474
Alexey Bondarenko <alexeybondarenko@me.com>
2015-10-26 10:04:59 +0200

    update script

--- bin/update.sh
+++ bin/update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+rm -rf generators/app/templates
 git clone --depth=1 --branch=master https://github.com/alexeybondarenko/simple-webapp.git generators/app/templates
-#git add -A
-#git commit -am "updated template"
-#git push
+git add -A
+git commit -am "updated template"
+git push
